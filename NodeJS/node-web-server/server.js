381763c37910b3c9872d2517ddfd9ee134b68dad
mirzasb <mirzasbaig@nyu.edu>
2017-08-09 10:07:31 -0400

    NodeJS Chapter 46

--- NodeJS/node-web-server/server.js
+++ NodeJS/node-web-server/server.js
@@ -1,21 +1,59 @@
 const express = require('express');
+const fs = require('fs');
+const hbs = require('hbs');
 
 var app = express();
 
-app.get('/', (req, res) => {
-        //res.send('<h1>Hello Express</h1>');
-        res.send({
-        name: 'Andrew',
-        likes: [
-            'Biking',
-            'Cities'
-        ]
-    }
-)
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.');
+        }
+    });
+    next();
 });
 
+app.use('/maintenance', (req, res, next) => {
+    res.render('maintenance.hbs', {
+        errorMessage: 'The page is being updated. Please try again later.'
+    });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+   return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+        //res.send('<h1>Hello Express</h1>');
+        // res.send({
+        // name: 'Andrew',
+        // likes: [
+        //     'Biking',
+        //     'Cities'
+        // ]
+        res.render('home.hbs', {
+            pageTitle: 'Homepage',
+            welcomeTxt: 'Welcome to the Homepage'
+        });
+    }
+);
+
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
 });
 
 app.get('/bad', (req, res) => {
@@ -24,4 +62,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
